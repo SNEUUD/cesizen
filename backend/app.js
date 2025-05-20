3b5cc5bda21b8f788733c16e08d6d2ba9b7e8655
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2025-05-20 20:35:03 +0200

    backend and git-ignore update

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données :", err);
+  } else {
+    console.log("Connecté à la base de données MySQL !");
+  }
+});
